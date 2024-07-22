1e46b7f5986a6cbea6d422a79ed2ee1e79316ac3
Tarun Jangra <111531292+tarunjangra043@users.noreply.github.com>
2024-07-22 08:40:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function getFormvalue() {
     //Write your code here
+	let form = document.getElementById('form1');
 
+	let firstName = form.elements['fname'].value;
+	let lastName = form.elements['lname'].value;
+
+	alert(`${firstName} ${lastName}`)
 }
